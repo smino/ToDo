8156d0aa420ef333a0cec455754d1908345fad13
Ruben Maurer <rubenmaurer@live.de>
2017-12-30 13:52:55 +0100

    Add DatabaseManager
    Add the first version of the DatabaseManager and the SettingsController.

--- src/main/java/de/swtproject/todo/core/database/DatabaseManager.java
+++ src/main/java/de/swtproject/todo/core/database/DatabaseManager.java
@@ -1,35 +1,115 @@
 package de.swtproject.todo.core.database;
 
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
 import de.swtproject.todo.core.ToDo;
+import de.swtproject.todo.util.Settings;
 
+import java.io.IOException;
 import java.sql.SQLException;
-import java.util.List;
+import java.util.Collection;
 
+/**
+ * Manager controller for accessing the database.
+ * Used to store and receive objects from the databasse.
+ *
+ * @author Ruben Maurer
+ * @version 1.0
+ * @since 0.1
+ */
 public class DatabaseManager {
 
+    /**
+     * The connection source used by the manager.
+     */
     private ConnectionSource connectionSource;
 
+    /**
+     * The database access object used by the manager.
+     */
+    private Dao<ToDo,String> databaseAccess;
+
+    /**
+     * DatabaseManager singleton.
+     */
     private static DatabaseManager databaseManager = new DatabaseManager();
 
+    /**
+     * Get the database manager.
+     *
+     * @return the database manager
+     */
     public static DatabaseManager getManager() {
         return databaseManager;
     }
 
+    /**
+     * Constructor for a new DatabaseManager
+     */
     private DatabaseManager() {
         try {
-            connectionSource = new JdbcConnectionSource("");
+            String conString = Settings.getConnectionString().intern();
+
+            connectionSource = new JdbcConnectionSource(conString);
+            databaseAccess = DaoManager.createDao(connectionSource, ToDo.class);
+
+            TableUtils.createTableIfNotExists(connectionSource, ToDo.class);
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public ToDo loadToDo() {
-        return null;
+    /**
+     * Close the used connection source.
+     *
+     * @throws IOException on I/O exception
+     */
+    public void destroy() throws IOException {
+        connectionSource.close();
     }
 
-    public List<ToDo> loadToDos() {
-        return null;
+    /**
+     * Save a single object.
+     *
+     * @param todo the object to save
+     * @throws SQLException on SQL exception
+     */
+    public void saveToDo(ToDo todo) throws SQLException {
+        databaseAccess.createIfNotExists(todo);
+    }
+
+    /**
+     * Saves a collection of objects
+     *
+     * @param collection the collections to save
+     * @return the status
+     * @throws SQLException on SQL exception
+     */
+    public int saveToDos(Collection<ToDo> collection) throws SQLException {
+        return databaseAccess.create(collection);
+    }
+
+    /**
+     * Load a single object from the database.
+     *
+     * @param id the string to query with
+     * @return the loaded object
+     * @throws SQLException on SQL exception
+     */
+    public ToDo loadToDo(String id) throws SQLException {
+        return databaseAccess.queryForId(id);
+    }
+
+    /**
+     * Load all object from database.
+     *
+     * @return the loaded objects
+     * @throws SQLException on SQL exceptions
+     */
+    public Collection<ToDo> loadAllToDo() throws SQLException {
+        return databaseAccess.queryForAll();
     }
 }
