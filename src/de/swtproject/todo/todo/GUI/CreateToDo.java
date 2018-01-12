0f69aca74c856f7a2b24d9e82b131ca095038d35
smino <ccsminocc@gmail.com>
2018-01-12 10:53:35 +0200

    newGui

---
+++ src/de/swtproject/todo/todo/GUI/CreateToDo.java
@@ -0,0 +1,20 @@
+package de.swtproject.todo.GUI;
+
+import javax.swing.*;
+
+public class CreateToDo extends JFrame{
+
+    private JPanel Panel;
+    private JPanel mainPanel;
+    private JPanel RightPanel;
+    private JComboBox intervalComboBox;
+    private JPanel LeftPanel;
+    private JTextField titelTextField;
+    private JTextField descriptionTextField;
+    private JComboBox milestonesComboBox;
+    private JLabel createToDoLabel;
+    private JLabel startLabel;
+    private JLabel descriptionLabel;
+    private JLabel milestonesLabel;
+    private JButton cancelButton;
+}
---
+++ src/de/swtproject/todo/todo/GUI/Mainsite/GuiMainsite.java
@@ -0,0 +1,39 @@
+package de.swtproject.todo.GUI.Mainsite;
+
+import javax.swing.*;
+
+public class GuiMainsite extends JFrame {
+
+
+
+    private JPanel Panel;
+    private JPanel LeftPanel;
+    private JPanel OptionPanel;
+    private JList list1;
+    private JPanel LeftButtonPanel;
+    private JButton archButton;
+    private JButton prodButton;
+    private JPanel FilterPanel;
+    private JComboBox comboBox1;
+    private JPanel MilestonePanel;
+    private JComboBox comboBox2;
+    private JList todoList;
+    private JPanel RightPanel;
+    private JPanel RightButtonPanel;
+    private JButton editButton;
+    private JButton finishButton;
+    private JButton deleteButton;
+    private JTextArea descriptionTextArea;
+    private JPanel StartDeadlinePanel;
+    private JPanel seadlineDatePanel;
+    private JPanel startPanel;
+    private JPanel MilestoneButtonPanel;
+    private JLabel Milestone;
+    private JPanel MilestoneLabelPanel;
+    private JTextField titleTextField;
+    private JScrollPane todoScrollPane;
+
+
+
+
+}
